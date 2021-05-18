11e29097fa83f62700e66c257891929d8bf56978
珍奶 <jary90705@gmail.com>
2021-05-18 17:27:28 +0800

    project init

---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,13 @@
+// 註解 -- 左邊的選單區
+export default [
+  {
+    title: '首頁Home',
+    route: 'home',
+    icon: 'HomeIcon',
+  },
+  {
+    title: 'Second Page',
+    route: 'second-page',
+    icon: 'FileIcon',
+  },
+]
